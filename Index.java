48c7de18b6a61903dc933231ef692fdc0aa3567a
Paulo <paulo.trimpler@escola.pr.gov.br>
2026-09-05 11:23:14 -0300

    Modificação do arquivo Index

--- Index.java
+++ Index.java
@@ -1,17 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Index {
     public static void main(String[] args) {
 
         // Variáveis 
 
-        
+
 
         // Variáveis;
 
+        // Listas
+
+        ArrayList<Aparelho> eletrodomesticos = new ArrayList<> ();
+
+        // Listas;
+
         // Lógica
 
+        eletrodomesticos.add(Televisao());
+        
+        for (int i = 0; i < eletrodomesticos.size(); i++) {
 
+            eletrodomesticos.get(i).Ligar();
+
+        }
 
         // Lógica;
 
     }
+
+    public static void print(String mensagem) {
+
+        System.out.println(mensagem);
+
+    }
+
+}
+
+class Aparelho {
+
+    private String nome;
+
+    Aparelho(String nomeRecebido) {
+
+        this.nome = nomeRecebido;
+
+    }
+
+    public void Ligar() {
+
+        Index.print("Ligando o aparelho " + nome);
+
+    }
+
+    public String getNome() {
+
+        return nome;
+
+    }
+
+}
+
+class Televisao extends Aparelho {
+
+    public void Televisao(String nomeRecebido) {
+
+        super(nome) = nomeRecebido;
+
+    }
+
+    @Override
+    public void Ligar() {
+
+        super.Ligar();
+        Index.print("Sintonizando canais...");
+
+    }
+
 }
